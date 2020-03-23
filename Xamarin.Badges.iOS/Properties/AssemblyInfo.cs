bcd39e14729fbf0684873e038db8c139f936dedb
Valentin Jubert <valentin.jubert@ideine.fr>
2020-03-23 10:04:00 +0100

    tentative correction interface iOS

--- Xamarin.Badges.iOS/Properties/AssemblyInfo.cs
+++ Xamarin.Badges.iOS/Properties/AssemblyInfo.cs
@@ -1,36 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xamarin.Badges.iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xamarin.Badges")]
+[assembly: AssemblyDescription("Access to application icon badge for Xamarin iOS")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xamarin.Badges.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Ideine")]
+[assembly: AssemblyProduct("Xamarin.Badges")]
+[assembly: AssemblyCopyright("Copyright © Ideine 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d07d6cf5-d18e-44c5-a128-9b89defee357")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
