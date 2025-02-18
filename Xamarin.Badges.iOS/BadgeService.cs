2b329271cd23718f72c305867a49c7d10fed20b3
Armand D <armand.deffrennes@ideine.fr>
2025-02-18 11:23:23 +0100

    iOS

--- Xamarin.Badges.iOS/BadgeService.cs
+++ Xamarin.Badges.iOS/BadgeService.cs
@@ -1,25 +1,24 @@
 ﻿using UIKit;
 
-namespace Xamarin.Badges.iOS
-{
-	public class BadgeService : IBadgeService
-	{
-		public void Clear()
-		{
-			UIApplication.SharedApplication.InvokeOnMainThread(() =>
-			{
-				//do not inline
-				UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
-			});
-		}
+namespace Xamarin.Badges;
 
-		public void Set(int badgeCount)
+public class BadgeService : IBadgeService
+{
+	public void Clear()
+	{
+		UIApplication.SharedApplication.InvokeOnMainThread(() =>
 		{
-			UIApplication.SharedApplication.InvokeOnMainThread(() =>
-			{
-				//do not inline
-				UIApplication.SharedApplication.ApplicationIconBadgeNumber = badgeCount;
-			});
-		}
+			//do not inline
+			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
+		});
+	}
+
+	public void Set(int badgeCount)
+	{
+		UIApplication.SharedApplication.InvokeOnMainThread(() =>
+		{
+			//do not inline
+			UIApplication.SharedApplication.ApplicationIconBadgeNumber = badgeCount;
+		});
 	}
 }
